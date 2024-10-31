96ca6bb417e92dbb4d3dad6632792109e1b363d7
emmpup <71734856+emmpup@users.noreply.github.com>
2024-10-31 14:27:52 -0700

    Update problem.js with a function that solves the problem stated

--- problem.js
+++ problem.js
@@ -13,3 +13,34 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+// create a variable to store glitched password string
+let glitchedPassword =
+  "This password you set should no be used because it glitches the system.";
+
+// make function with params of password and usersInput
+function checkPassword(password, usersInput) {
+  
+  // conditional statement if the password and user's inputed password are the same, log out "Access Granted!"
+  if (password === usersInput) {
+    return console.log("Access Granted!");
+
+    // else if the user's inputed password is forgot, log out "Here is a hint", and glitchedPassword
+  } else if (usersInput === "forgot") {
+    return console.log(`Here is a hint: ${glitchedPassword}`);
+
+    // else if the user's inputed password is reset, log out "let's reset your account", and glitchedPassword
+  } else if (usersInput === "reset") {
+    return console.log("Let's reset your account: " + glitchedPassword);
+
+    // else if user's inputed password has less than 5 characters, log out "Your password is too short!"
+  } else if (usersInput.length < 5) {
+    return console.log("Your password is too short!");
+
+    // else log "Access Denied!"
+  } else {
+    return console.log("Access Denied!");
+  }
+}
+
+// Checks to see if the function runs correctly
+checkPassword("hello", "doodle");
